602134747431962312eab188ee3bc708253edf54
jeetisngh-621 <jeetsinghjeet68@gmail.com>
2025-01-09 10:40:50 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Step from './Step';
 
 const App = () => {
+  const [currentstep, setcurrentstep] = useState(1);
+  const [formdata, setformdata] = useState([{
+    first_name : "",
+    last_name:"",
+    model:"",
+    car_price:"",
+    card_info:"",
+    expiry_date:"",  
+  }]);
+
+  const handlenext=()=>setcurrentstep((prev)=>prev+1);
+  const handleprev =()=>setcurrentstep((prev)=>prev-1);
+
+  const handlesubmit=(e)=>{
+    e.preventDefault();
+    console.log("form submitted:", formdata);
+  };
+
+  const handleinputchange=(e)=>{
+    const {id , value} = e.target;
+    setformdata((prevdata)=>({
+      ...prevdata,
+      [id]:value,
+    }));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div>
+          <Step step={currentstep} formdata={formdata} handleinputchange={handleinputchange} handlenext={handlenext}
+            handleprev={handleprev} handlesubmit={handlesubmit}
+          />
+
+      </div>
     </div>
   )
 }
---
+++ src/components/Step.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Step = ({step,handlesubmit,handleprev,handlenext,formdata,handleinputchange}) => {
+  return (
+    <div>
+
+        <form onSubmit={handlesubmit}>
+           {step === 1 && ( <div>
+                <h2>Customer Detail</h2>
+                <label htmlFor="">First Name:</label>
+            <input type="text"  required id='first_name' value={formdata.first_name} onChange={handleinputchange}/>
+            <br /><br />
+            <label htmlFor="">Last Name:</label>
+            <input type="text" name="" id="last_name" required value={formdata.last_name} onChange={handleinputchange} />
+            </div>)}
+
+          {step === 2 && (
+              <div>
+              <h2>Car Detail</h2>
+              <label htmlFor="">Car Model:</label>
+              <input type="text" name="" id="model" value={formdata.model} onChange={handleinputchange} required />
+              <br /> <br />
+              <label htmlFor="">Car Price:</label>
+              <input type="text" name="" id="car_price" value={formdata.car_price}  onChange={handleinputchange} required/>
+          </div>
+          )}
+
+        {step === 3 &&(
+                <div>
+                <h2>Payment Detail </h2>
+                <label htmlFor="">Card Info:</label>
+                <input type="text" id='card_info' value={formdata.card_info} required />
+                <br /> <br />
+                <label htmlFor="">Expiry Date:</label>
+                <input type="date" name="" id="expiry_date" required value={formdata.expiry_date} onChange={handleinputchange} />
+            </div>
+        )}
+                <br /><br />
+            <div>
+                {step>1 && (<button onClick={handleprev} type='button'>Previous</button>)}
+
+               {step<3 && ( <button onClick={handlenext} type='button'>Next</button>)}
+
+               {step ===3 && (<button type='submit'>Submit</button> )}
+
+            </div>
+
+
+        </form>
+    </div>
+  )
+}
+
+export default Step
